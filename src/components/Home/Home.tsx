f970c335e0841a4cf6cbb0da2a5830fcfdca7108
Adrian Ferrera <adrian.afergon@gmail.com>
2020-06-21 12:27:53 +0100

    refactor: create builder to repository and share constant

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { ProductsRepository } from '../../repositories/ProductsRepository';
 import { Product } from '../../models/product';
 
+export enum HomeText {
+  emptyMessage = 'No products were found'
+}
+
 interface HomeProps {
   productsRepository: ProductsRepository;
 }
@@ -15,7 +19,7 @@ export const Home: React.FC<HomeProps> = ({ productsRepository }) => {
     <section>
       { products
         ? products.map(product => <article key={product.handle}>{product.title}</article>)
-        : <p>No products were found</p>
+        : <p>{HomeText.emptyMessage}</p>
       }
     </section>
   );
